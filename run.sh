7246e2f182e2f772ea6bc16ecd25add1580a26e7
Tr1ppyP <147446785+Tr1ppyP@users.noreply.github.com>
2024-03-18 19:38:30 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pip3 install ansible
-echo 'export PATH="$PATH:/home/kali/.local/bin"' >> ~/.zshrc; source ~/.zshrc
+#echo 'export PATH="$PATH:/home/kali/.local/bin"' >> ~/.zshrc; source ~/.zshrc
 sleep 1
 git clone https://github.com/Tr1ppyP/kali-ansible.git
 cd kali-ansible
