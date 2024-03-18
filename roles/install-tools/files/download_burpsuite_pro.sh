388841a67a4fd4e725b05b9a610a0c789b048530
Tr1ppyP <147446785+Tr1ppyP@users.noreply.github.com>
2024-03-18 18:21:55 +0000

    Update download_burpsuite_pro.sh

--- roles/install-tools/files/download_burpsuite_pro.sh
+++ roles/install-tools/files/download_burpsuite_pro.sh
@@ -5,7 +5,7 @@ chmod +x burpsuite_pro_linux.sh
 bash burpsuite_pro_linux.sh &
 sleep 5
 xdotool key Return; sleep 2; xdotool key Tab Tab Tab Return; sleep 2; xdotool key Return; sleep 2; xdotool key Tab Tab Tab Return; sleep 2; xdotool key Tab Tab Tab Return; sleep 30; xdotool key Tab Return
-xsel --clipboard < "/mnt/hgfs/Scripts/Burp_Suite_Professional_-_licensed_to_Six_Degrees_[11_user_license].txt"
+#copy_burp_license
 bash /home/kali/BurpSuitePro/BurpSuitePro &
 sleep 5
 xdotool key Tab Control_L+v Return
