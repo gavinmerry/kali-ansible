a002d69720e1022a6aec91febd17f45aa4a0dc68
Tr1ppyP <147446785+Tr1ppyP@users.noreply.github.com>
2024-03-18 19:38:58 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,6 @@
 
 pip3 install ansible
 #echo 'export PATH="$PATH:/home/kali/.local/bin"' >> ~/.zshrc; source ~/.zshrc
-sleep 1
-git clone https://github.com/Tr1ppyP/kali-ansible.git
-cd kali-ansible
 ansible-galaxy install -r requirements.yml
 sudo whoami
 ansible-playbook main.yml
