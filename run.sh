6fc0bc778b8f4a183e9fbd38620416b80f1735b3
trippyp <gavinmerry93@gmail.com>
2024-03-19 15:48:01 +0000

    commit

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 pip3 install ansible
+pip3 install jmespath
 #echo 'export PATH="$PATH:/home/kali/.local/bin"' >> ~/.zshrc; source ~/.zshrc
 ansible-galaxy install -r requirements.yml
 sudo whoami
