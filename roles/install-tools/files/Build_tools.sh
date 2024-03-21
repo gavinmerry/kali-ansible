46959b87228d7e38bfb946433f4b8ab2658cf571
trippyp <gavinmerry93@gmail.com>
2024-03-21 10:54:28 +0000

    commit

--- roles/install-tools/files/Build_tools.sh
+++ roles/install-tools/files/Build_tools.sh
@@ -2,7 +2,7 @@
 
 searchsploit -u
 bash /opt/linWinPwn/install.sh
-dpkg -i /opt/pet/*_linux_amd64.deb
+#dpkg -i /tmp/pet/*_linux_amd64.deb
 
 #append pet bind key to the end of .zshrc
-echo 'function pet-select() {' >> ~/.zshrc && echo '  BUFFER=$(pet search --query "$LBUFFER")' >> ~/.zshrc && echo '  CURSOR=$#BUFFER' >> ~/.zshrc && echo '  zle redisplay' >> ~/.zshrc && echo '}' >> ~/.zshrc && echo 'zle -N pet-select' >> ~/.zshrc && echo 'stty -ixon' >> ~/.zshrc && echo 'bindkey "^s" pet-select' >> ~/.zshrc
+#echo 'function pet-select() {' >> ~/.zshrc && echo '  BUFFER=$(pet search --query "$LBUFFER")' >> ~/.zshrc && echo '  CURSOR=$#BUFFER' >> ~/.zshrc && echo '  zle redisplay' >> ~/.zshrc && echo '}' >> ~/.zshrc && echo 'zle -N pet-select' >> ~/.zshrc && echo 'stty -ixon' >> ~/.zshrc && echo 'bindkey "^s" pet-select' >> ~/.zshrc
