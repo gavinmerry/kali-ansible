b67638f7924c77f88bb8a4b5d77efd7f061e1790
trippyp <gavinmerry93@gmail.com>
2024-04-05 18:34:09 +0100

    commit

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ pip3 install jmespath
 #echo 'export PATH="$PATH:/home/kali/.local/bin"' >> ~/.zshrc; source ~/.zshrc
 ansible-galaxy install -r requirements.yml
 sudo whoami
-ansible-playbook main.yml
+ansible-vault decrypt /mnt/hgfs/License_files/secret_vars.yml && ansible-playbook main.yml
